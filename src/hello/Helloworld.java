b2da4a047841cc061f05851e039dcfef9b360aac
naveen <naveenkumarpenikala@gmail.com>
2025-06-20 21:31:04 +0530

    Initial commit

---
+++ src/hello/Helloworld.java
@@ -0,0 +1,16 @@
+package hello;
+public class Helloworld {
+
+    public void printNumbers()
+    {
+        for(int i=0;i<=10;i++) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        Helloworld hy = new Helloworld();
+        hy.printNumbers();
+    }
+}
