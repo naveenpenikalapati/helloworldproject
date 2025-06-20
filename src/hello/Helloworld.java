6d1b52e1928c8fd182f75a8707d858fb2cde07e0
naveen <naveenkumarpenikala@gmail.com>
2025-06-20 22:25:23 +0530

    second commit

--- src/hello/Helloworld.java
+++ src/hello/Helloworld.java
@@ -7,10 +7,17 @@ public class Helloworld {
             System.out.println(i);
         }
     }
+    public void printNumbers1()
+    {
+        for(int i=0;i<=20;i++) {
+            System.out.println(i);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
 
         Helloworld hy = new Helloworld();
         hy.printNumbers();
+        hy.printNumbers1();
     }
 }
